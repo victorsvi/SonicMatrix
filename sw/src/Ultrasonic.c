70fbff9d1ea498f1b8c49c64f4fd7c77637b4324
victorsvi <victorsvi@gmail.com>
2020-03-14 01:38:28 -0300

    Phase calculation and Dump funcion

--- sw/src/Ultrasonic.c
+++ sw/src/Ultrasonic.c
@@ -20,10 +20,9 @@
 //#include <EEPROM.h>
 //#include <Arduino.h>
 
-
 #define SOUND_SPEED 343600 //speed of the sound in the medium in milimeters per second (at 20°C dry air)
 #define SOUND_FREQ 40000 //frequency of the signal
-//#define RESOLUTION 8 //in how many discrete steps the period of the signal is divided. It defines the phase delay resolution and the amplitude resolution
+#define SOUND_WAVELEN SOUND_SPEED/SOUND_FREQ //wave length (8.6 mm)
 
 uint8_t transd_array_init( t_transd_array *transd_array, const uint8_t size_x, const uint8_t size_y, const uint8_t elem_diameter, const uint8_t elem_separation, const uint8_t phase_res ){
 	
@@ -91,7 +90,7 @@ uint8_t transd_array_set( t_transd_array *transd_array, const uint8_t index_x, c
 	return ret;
 } //transd_array_set
 
-uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z ) {
+uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t duty_cycle, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z ) {
 	
 	uint8_t ret;
 	uint8_t x,y;
@@ -104,7 +103,7 @@ uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t focu
 	for(x = 0; x < size_x; x++){
 		for(y = 0; y < size_y; y++){
 			
-			ret = transd_calcfocus( (transd_array->transd_ptr + x * size_y + y), /*transd_array->phase_res, transd_array->curr_step,*/ focus_x, focus_y, focus_z );
+			ret = transd_calcfocus( (transd_array->transd_ptr + x * size_y + y), transd_array->phase_res, /*transd_array->curr_step,*/ focus_x, focus_y, focus_z );
 			if(ret != 0) {
 				return ret;
 			}
@@ -114,7 +113,7 @@ uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t focu
 	return 0;
 } //transd_array_calcfocus
 
-uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t amplitude ) {
+uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t duty_cycle ) {
 	
 	uint8_t ret;
 	uint8_t x,y;
@@ -127,7 +126,7 @@ uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t ampli
 	for(x = 0; x < size_x; x++){
 		for(y = 0; y < size_y; y++){
 			
-			ret = transd_calcflat( (transd_array->transd_ptr + x * size_y + y), /*transd_array->phase_res, transd_array->curr_step,*/ amplitude );
+			ret = transd_calcflat( (transd_array->transd_ptr + x * size_y + y), transd_array->phase_res, /*transd_array->curr_step,*/ duty_cycle );
 			if(ret != 0) {
 				return ret;
 			}
@@ -137,12 +136,71 @@ uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t ampli
 	return 0;
 } //transd_array_calcflat
 
+#IFDEF DEBUG
+/* Dumps the array structure to JSON */
+void transd_dumpToFile ( FILE *f,  t_transd_array *transd_array ){
+	
+	char buff[1024];
+	char patt[16];
+	t_transd *transd_ptr;
+	uint8_t i,x,y;
+	
+	if(f == NULL) return;
+	
+	sprintf(buff, "{\r\n\t\"size_x\": %u,\r\n\t\"size_y\": %u,\r\n\t\"elem_diameter\": %u,\r\n\t\"elem_separation\": %u,\r\n\t\"phase_res\": %u,\r\n\t\"transd_ptr\": [\r\n", transd_array->size_x, transd_array->size_y, transd_array->elem_diameter, transd_array->elem_separation, transd_array->phase_res);
+	fputs(buff, f);
+	
+	for(x = 0; x < size_x; x++){
+		for(y = 0; y < size_y; y++){
+			
+			if( x == 0 && y == 0 ) {
+				fputc(',',f);
+			}
+			
+			transd_ptr = (transd_array->transd_ptr + x * size_y + y);
+			
+			for(i = 0; i < 16; i++) {
+				
+				patt[i] = transd_ptr->pattern & ((1 << (i+1)) - 1) ? '#' : '_';
+			}
+			
+			sprintf(buff, "\t{\r\n\t\t\"port_pin\": %u,\r\n\t\t\"phase_comp\": %u,\r\n\t\t\"x\": %u,\r\n\t\t\"y\": %u,\r\n\t\t\"phase\": %u,\r\n\t\t\"duty_cycle\": %u,\r\n\t\t\"pattern\": \"%s\"\r\n\t}", transd_ptr->port_pin, transd_ptr->phase_comp, transd_ptr->x, transd_ptr->y, transd_ptr->phase, transd_ptr->duty_cycle, patt);
+			fputs(buff, f);
+
+		}
+	}
+	
+	sprintf(buff, "\t]\r\n}\r\n", );
+	fputs(buff, f);
+}
+#ENDIF
 
 
 
 /* PRIVATE */
 
-uint8_t transd_getPattern( const uint8_t phase_res, const uint8_t phase_comp, const uint8_t phase, const uint8_t duty ){
+uint32_t transd_sqrt_int (const uint32_t x) {
+
+    uint32_t l = 1;
+    uint32_t h = x;
+    uint32_t n = (l + h)/2;
+
+    while ( l < n ){
+
+        if( (n * n) <= x ){
+            l = n;
+        }
+        else if ( (n * n) > x ) {
+            h = n;
+        }
+        n = (l + h)/2;
+
+    };
+
+    return l;
+}
+
+uint16_t transd_getPattern( const uint8_t phase_res, const uint8_t phase_comp, const uint8_t phase, const uint8_t duty ){
 	/*
 	RESOLUTION: Define em quantos steps um período será dividido (quantos bits terá o padrão e cara transdutor). Para uma coordenada, armazenar o padrão em um inteiro para cada transd
 	para gerar o sinal, basta ler os bits sequencialmente
@@ -165,7 +223,7 @@ uint8_t transd_getPattern( const uint8_t phase_res, const uint8_t phase_comp, co
 		2   -> ___#####__ (72°)
 		8   -> ##_____### (288°) (5 amp, 8 ph)
 	*/
-	bits_phase = (phase / (255 / resolução));
+	bits_phase = ((phase + phase_comp) / (255 / phase_res));
 	
 	//sets the (Nth + 1) bit = 1 (00001000b for n = 3) then subtracts one to make all the lesser bits = 1 (00000111b for n = 3) 
 	pattern = (1 << bits_duty) - 1; 
@@ -173,14 +231,12 @@ uint8_t transd_getPattern( const uint8_t phase_res, const uint8_t phase_comp, co
 	//shifts the duty cycle pattern to change phase
 	pattern <<= bits_phase;
 	
-	//if phase shifts more bits than the duty cycle bits, the pattern lost some duty cycle information (the bits that overflow when shifting) 
-	if(bits_phase > bits_duty) {
-		//generates
-		pattern |= (1 << (bits_phase - bits_duty)) - 1; 
+	//if shifting bit for phase "overflows" the number of bits of the pattern (defined by the resolution), the pattern lost some duty cycle information (the bits that overflow when shifting)
+	if(bits_phase + bits_duty > phase_res) {
+		//generates the bits that were lost on phase shitfing at the LSB side
+		pattern |= (1 << (bits_duty + bits_phase - phase_res)) - 1;
 	}
-	
-	
-	
+		
 	return pattern;
 }
 
@@ -201,7 +257,7 @@ uint8_t transd_init( t_transd *transd, const uint8_t index_x, const uint8_t inde
 	transd->x = ((elem_diameter / 2) + (elem_diameter * index_x) + (elem_separation * index_x)); //calculates the position of the transducer on the array
 	transd->y = ((elem_diameter / 2) + (elem_diameter * index_y) + (elem_separation * index_y)); //calculates the position of the transducer on the array
 	transd->phase = 0;
-	transd->amplitude = 0;
+	transd->duty_cycle = 0;
 	/* here is an example of the calculation of the position in one axys (elem_diameter = 16, elem_separation = 2)
 	 
 	  diameter           separation
@@ -217,11 +273,50 @@ uint8_t transd_init( t_transd *transd, const uint8_t index_x, const uint8_t inde
 	 */
 } //transd_init
 
-uint8_t transd_set( t_transd *transd, uint8_t port_pin, uint8_t phase_comp );
+uint8_t transd_set( t_transd *transd, uint8_t port_pin, uint8_t phase_comp ) {
+	
+	if(transd == NULL) {
+		#IFDEF DEBUG DEBUG_MSG("The transducer pointer is null") #ENDIF
+		return 44;	
+	}
+	
+	transd->port_pin = port_pin;
+	transd->phase_comp = phase_comp;
+	
+	return 0;	
+} //transd_set
 
-uint8_t transd_calcfocus( t_transd *transd, /*const uint8_t phase_res, const uint8_t curr_step,*/ const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z );
+uint8_t transd_calcfocus( t_transd *transd, const uint8_t phase_res, /*const uint8_t curr_step,*/ const uint8_t duty_cycle, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z ){
+	
+	uint32_t distance;
+	int8_t Dx,Dy; //may be smaller than zero
+	
+	if(transd == NULL) {
+		#IFDEF DEBUG DEBUG_MSG("The transducer pointer is null") #ENDIF
+		return 44;	
+	}
+	
+	/*
+		W = S / f (wave length = sound speed / frequency)
 
-uint8_t transd_calcflat( t_transd *transd, /*const uint8_t phase_res, const uint8_t curr_step,*/ const uint8_t amplitude ) {
+		Dij = ( (Xij - x)^2 + (Yij - y)^2 + (-z)^2 )^1/2 (distance from the ij transducer to the focus point)
+
+		Pij = ( Dij * 256 ) / W  (phase of the ij transducer to have 0° of phase at the focus point) (testar o que acontece se Dij > W, já que Pij é uint8_t)(no pc funcionou!)
+	*/
+	Dx = transd->x - focus_x;
+	Dy = transd->y - focus_y;
+	distance = (Dx * Dx) + (Dy * Dy) + (focus_z * focus_z);
+	distance = transd_sqrt_int(distance);
+	distance = distance % SOUND_WAVELEN;
+	transd->phase = (uint8_t) (( distance * 256) / SOUND_WAVELEN); 
+	
+	transd->duty_cycle = duty_cycle;
+	transd->pattern = transd_getPattern( phase_res, transd->phase_comp, transd->phase, transd->duty_cycle ); 
+	
+	return 0;	
+} //transd_calcfocus
+
+uint8_t transd_calcflat( t_transd *transd, const uint8_t phase_res, /*const uint8_t curr_step,*/ const uint8_t duty_cycle ) {
 	
 	if(transd == NULL) {
 		#IFDEF DEBUG DEBUG_MSG("The transducer pointer is null") #ENDIF
@@ -229,24 +324,9 @@ uint8_t transd_calcflat( t_transd *transd, /*const uint8_t phase_res, const uint
 	}
 	
 	transd->phase = 0;
-	transd->amplitude = amplitude;
+	transd->duty_cycle = duty_cycle;
+	transd->pattern = transd_getPattern( phase_res, transd->phase_comp, transd->phase, transd->duty_cycle ); 
 	
-	
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return 0;	
+} //transd_calcflat
 
--- sw/src/Ultrasonic.h
+++ sw/src/Ultrasonic.h
@@ -8,37 +8,15 @@
 
 #include <stdint.h>
 
-
-///RESOLUTION: Define em quantos steps um período será dividido (quantos bits terá o padrão e cara transdutor). Para uma coordenada, armazenar o padrão em um inteiro para cada transd
-//para gerar o sinal, basta ler os bits sequencialmente
-//fazer a resolução variável, de no máximo 16 (16bits)
-/*
-duty -> define a quantidade de 1s e 0s (bits on = duty/resolução; 3 = 30/10) (se o duty for de 0 a 255, fazer bits on = (((duty*100)/ 255)/resolução))
-30% -> ###_______
-50% -> #####_____
-
-phase -> define quanto deslocar (bits deslocados = fase / (360/resolução); 2 = 72 / 360 / 10) (se a fase for de 0 a 255, fazer bits deslocados = (fase/(255)/resolução))
-0   -> #####_____
-2   -> __#####___ (72°)
-
-W = S / f (wave length = sound speed / frequency)
-
-Dij = ( (Xij - x)^2 + (Zij - z)^2 + (-z)^2 )^1/2
-
-Pij = ( Dij * 256 ) / W   (testar o que acontece se Dij > W, já que Pij é uint8_t)(no pc funcionou!)
-
-
-*/
-
 typedef struct s_transd {
 	uint8_t port_pin; //pin on which the transducer is connected
 	//uint8_t *port_bank; //pin's port bank
 	//uint8_t port_bankidx; //pin's index on port bank
-	uint8_t phase_comp; //physical phase delay  of the transducer
+	uint8_t phase_comp; //physical phase delay of the transducer. Use to compensate for individual 
 	uint8_t x; //x position of the center of the transducer in milimeters
 	uint8_t y; //y position of the center of the transducer in milimeters
-	uint8_t phase; //current discrete phase delay of the transducer (how many steps the signal will delay. Depends on the signal resolution)
-	uint8_t amplitude; //current discrete amplitude of the transducer (how many steps the signal will be on, like a duty cycle. Depends on the signal resolution)
+	uint8_t phase; //current discrete phase delay of the transducer. It ranges from 0 to 255 and equates to 0 to 2pi.
+	uint8_t duty_cycle; //current discrete duty cycle of the transducer. It ranges from 0 to 255 and equates to 0% to 100%.
 	uint16_t pattern;
 } t_transd;
 
@@ -47,7 +25,7 @@ typedef struct s_transd_array {
 	uint8_t size_y; //number of transducers in the y axys
 	uint8_t elem_diameter; //diameter of each transducer in milimeters
 	uint8_t elem_separation; //separation between transducers in milimeters
-	t_transd *transd_ptr; //pointer to the transducer list 
+	t_transd *transd_ptr; //pointer to the transducer array 
 	uint8_t phase_res;
 	/*uint8_t curr_step;*/
 } t_transd_array;
@@ -58,20 +36,13 @@ uint8_t transd_array_init( t_transd_array *transd_array, const uint8_t size_x, c
 
 uint8_t transd_array_set( t_transd_array *transd_array, const uint8_t index_x, const uint8_t index_y, const uint8_t port_pin, const uint8_t phase_comp );
 
-uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z );
+uint8_t transd_array_calcfocus( t_transd_array *transd_array, const uint8_t duty_cycle, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z );
 
-uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t amplitude );
+uint8_t transd_array_calcflat( t_transd_array *transd_array, const uint8_t duty_cycle );
 
-/* PRIVATE */
-
-//uint8_t transd_init( t_transd *transd, const uint8_t index_x, const uint8_t index_y, const uint8_t port_pin, const int8_t phase_comp, const uint8_t elem_diameter, const uint8_t elem_separation );
-
-//uint8_t transd_set( t_transd *transd, uint8_t port_pin, int8_t phase_comp );
-
-//uint8_t transd_calcfocus( t_transd *transd, const uint8_t phase_res, const uint8_t curr_step, const uint8_t focus_x, const uint8_t focus_y, const uint8_t focus_z );
-
-//uint8_t transd_calcflat( t_transd *transd, const uint8_t phase_res, const uint8_t curr_step, const uint8_t amplitude );
-
+#IFDEF DEBUG
+void transd_dumpToFile ( FILE *f,  t_transd_array *transd_array );
+#ENDIF
 
 #ifdef __cplusplus
 	}
