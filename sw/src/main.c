94a6d19efc578ce88b9785ed4c5698faed899c6f
Victor Salvi <victorsvi@gmail.com>
2020-03-29 02:37:12 -0300

    Update main.c

--- sw/src/main.c
+++ sw/src/main.c
@@ -63,6 +63,39 @@ struct s_state {
 	uint8_t lastx = 0, lasty = 0, lastz = 0;
 };
 
+{
+	if(isActiveMove || isActiveStatic) {
+		//state.lastx = state.x;
+		//state.lasty = state.y;
+		//state.lastz = state.z;
+		
+		moveCurrStep = 0;
+		moveNSteps = 0;
+		
+		//calculate output buffer
+		if(isActiveMove) { //movo to
+			
+		}
+		else if (!isFlat) { //focus static
+			
+		}
+		else { //flat
+			
+			transd_array_calcflat( transd_array, state.d );
+
+		}
+		
+		enableTimer4 ();
+		
+	}
+	
+	if(isStop) {
+		
+		disableTimer4 ();
+		
+	}
+}
+
 struct s_pin {
 	volatile uint8_t *bank_ptr;
 	uint8_t bit_idx;
@@ -72,8 +105,70 @@ struct s_pin {
 
 const uint8_t ARRAY_CALIBRATION[ARRAY_SIZE_X][ARRAY_SIZE_Y][2] = {
 	//pin numer, phase compensation
-	0, 0, //x0, y0
-	0, 0 //x0, y1
+	4 , 0,//x0, y0
+	6 , 0,//x0, y1
+	7 , 0,
+	8 , 0,
+	9 , 0,
+	10, 0,
+	11, 0,
+	12, 0,
+	13, 0,
+	14, 0,
+	15, 0,
+	16, 0,
+	17, 0,
+	18, 0,
+	19, 0,
+	20, 0,
+	21, 0,
+	22, 0,
+	23, 0,
+	24, 0,
+	25, 0,
+	26, 0,
+	27, 0,
+	28, 0,
+	29, 0,
+	30, 0,
+	31, 0,
+	32, 0,
+	33, 0,
+	34, 0,
+	35, 0,
+	36, 0,
+	37, 0,
+	38, 0,
+	39, 0,
+	40, 0,
+	41, 0,
+	42, 0,
+	43, 0,
+	44, 0,
+	45, 0,
+	46, 0,
+	47, 0,
+	48, 0,
+	49, 0,
+	50, 0,
+	51, 0,
+	52, 0,
+	53, 0,
+	55, 0,
+	56, 0,
+	57, 0,
+	58, 0,
+	59, 0,
+	60, 0,
+	61, 0,
+	62, 0,
+	63, 0,
+	64, 0,
+	65, 0,
+	66, 0,
+	67, 0,
+	68, 0,
+	69, 0
 };
 
 const struct s_pin PINS[70] = {
